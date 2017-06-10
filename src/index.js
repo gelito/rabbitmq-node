f7b44bf2f3a34ff383eff6e3d133b572b0b9ed26
gelito <angel.quesada@kubide.es>
2017-06-10 20:36:50 +0200

    Tested the amqp code

--- src/index.js
+++ src/index.js
@@ -1,4 +1,24 @@
 const debug = require('debug')('kubide:main-access');
+const amqp = require('interfaces/amqp');
 
-debug('hi', process.env.NODE_PATH);
-console.log('log', process.env.NODE_PATH);
+const senderChannel = process.env.PROCESS === '1' ? 'ping' : 'pong';
+const receiverChannel = process.env.PROCESS === '1' ? 'pong' : 'ping';
+
+debug(`Start game as process ${process.env.PROCESS} send to: ${senderChannel} receive from: ${receiverChannel}`);
+
+setTimeout(() => {
+  amqp.on(receiverChannel, (element) => {
+    debug(`received the ${element} from ${receiverChannel}`);
+    const nextMsg = Number(element) + 1;
+
+    debug(`emit to ${nextMsg} to ${senderChannel}`);
+    amqp.emit(senderChannel, nextMsg);
+  });
+}, 3000);
+
+if (process.env.PROCESS === '2') {
+  setTimeout(() => {
+    debug(`emit 1 to ${senderChannel}`);
+    amqp.emit(senderChannel, 1);
+  }, 10000);
+}
